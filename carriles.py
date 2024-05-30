e847faa6975215ecf6486bd09848804104ddb3c1
maxkigo <maximiliano.lopez@cargomovil.com>
2024-05-30 10:05:35 -0600

    updated bug of matplot

--- carriles.py
+++ carriles.py
@@ -1,8 +1,5 @@
 import streamlit as st
 import pandas as pd
-import numpy as np
-import matplotlib.pyplot as plt
-import seaborn as sns
 from google.cloud import bigquery
 from google.oauth2 import service_account
 
